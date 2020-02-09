60b73278166ee2d4dc1bcd648258a0bb3b1bf543
sycured <60801403+sycured@users.noreply.github.com>
2020-02-09 15:13:25 -0500

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# streaming_calc
+Bandwidth calculation for streaming server
+
+## Usage
+
+### Determine necessary server bandwidth
+
+    ./streaming_calc.py bwserver nblisteners bitrate
+
+    ./streaming_calc.py bwserver 250 64
+
+**Output**
+
+    Number of listeners : 250
+    Bitrate (kb/s) : 64
+    Server bandwidth (Mib/s) : 15625.0
+
+
+### Determine the amount of data used for the streaming
+
+    ./streaming_calc.py usagebw nblisteners bitrate nbdays nbhours
+
+    ./streaming_calc.py usagebw 250 64 1 24
+
+**Output**
+
+    Number of listeners : 250
+    Bitrate (kb/s) : 64
+    Number of days : 1
+    Number of hours by days : 24
+    Bandwidth used (GiB) : 164794.921875
---
+++ streaming_calc.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""Bandwidth calculation for streaming server."""
+
+from argparse import ArgumentParser
+from sys import argv, exit
+
+
+def bw_server(args_bw_server):
+    """Determine necessary server bandwidth."""
+    sum_bw_server = \
+        args_bw_server.nblisteners * args_bw_server.bitrate * 1000 / 1024
+    print(f"""Number of listeners: {args_bw_server.nblisteners}
+Bitrate (kb/s): {args_bw_server.bitrate}
+Server bandwidth (Mib/s): {sum_bw_server}""")
+
+
+def server_usage_bw(args_server_usage_bw):
+    """Determine the amount of data used for the streaming."""
+    sum_server_usage_bw = \
+        args_server_usage_bw.nbdays * args_server_usage_bw.nbhours * \
+        3600 * args_server_usage_bw.bitrate * 1000 / 8 * \
+        args_server_usage_bw.nblisteners / 1024 / 1024
+    print(f"""Number of listeners: {args_server_usage_bw.nblisteners}
+Bitrate (kb/s): {args_server_usage_bw.bitrate}
+Number of days: {args_server_usage_bw.nbdays}
+Number of hours by days: {args_server_usage_bw.nbhours}
+Bandwidth used (GiB): {sum_server_usage_bw}""")
+
+
+def main():
+    """Main function with parsing arguments."""
+    parser = ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    parser_bwserver = subparsers.add_parser(
+        'bwserver',
+        help='Determine necessary server bandwidth'
+    )
+    parser_bwserver.add_argument(
+        'nblisteners',
+        type=int,
+        help='number of listeners'
+    )
+    parser_bwserver.add_argument(
+        'bitrate',
+        type=int,
+        help='bitrate in kb/s'
+    )
+    parser_bwserver.set_defaults(func=bw_server)
+
+    parser_serverusagebw = subparsers.add_parser(
+        'usagebw',
+        help='Determine the amount of data used for the streaming'
+    )
+    parser_serverusagebw.add_argument(
+        'nblisteners',
+        type=int,
+        help='number of listeners')
+    parser_serverusagebw.add_argument(
+        'bitrate',
+        type=int,
+        help='bitrate in kb/s')
+    parser_serverusagebw.add_argument(
+        'nbdays',
+        type=int,
+        help='number of days')
+    parser_serverusagebw.add_argument(
+        'nbhours',
+        type=int,
+        help='number of hours by days (integer only)')
+    parser_serverusagebw.set_defaults(func=server_usage_bw)
+
+    args = parser.parse_args()
+    if len(argv) == 1:
+        parser.print_help()
+        exit(1)
+    args.func(args)
+
+
+if __name__ == '__main__':
+    main()
